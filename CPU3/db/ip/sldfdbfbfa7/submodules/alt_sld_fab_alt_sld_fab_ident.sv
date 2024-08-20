bbd6c78408f90433b9260ffd5e4bd97ebca5bff8
olman2020 <irodriguezcr1@gmail.com>
2024-08-19 20:55:14 -0600

    Solucion

--- CPU3/db/ip/sldfdbfbfa7/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ CPU3/db/ip/sldfdbfbfa7/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : 3ec30146e6a0adbfcb11
+//   DESIGN_HASH   : fde60af70cb49ddd0abc
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h3ec30146e6a0adbfcb11 };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'hfde60af70cb49ddd0abc };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
